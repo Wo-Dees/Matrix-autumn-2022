09b9ce265c40c6e0888b948b4850a66cbbf1f9ab
Леонид Переверзин <leonidpereverzin@MacBook-Pro-Leonid.local>
2022-11-11 20:56:50 +0300

    test

--- tests/MatrixTest.cpp
+++ tests/MatrixTest.cpp
@@ -1,3 +1,6 @@
-//
-// Created by Леонид Переверзин on 11.11.2022.
-//
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
